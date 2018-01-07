d2b668e1aa8728b3ea40d2b26f0292327d0e6bc9
Koki Matsuzawa <shingaryu@gmail.com>
2018-01-07 16:40:29 +0900

    USUM対応の現行世代に対応するまで暫定的に6世代対戦で運用

--- roomhandler.js
+++ roomhandler.js
@@ -122,7 +122,7 @@ function recieve(data) {
 			var challenges = JSON.parse(data.substr(18));
 			if(challenges.challengesFrom) {
 				for(var user in challenges.challengesFrom) {
-					if(challenges.challengesFrom[user] == "gen7randombattle") {
+					if(challenges.challengesFrom[user] == "gen6randombattle") {
 						logger.info("Accepting challenge from " + user);
 						send("/accept " + user);
 					} else {
