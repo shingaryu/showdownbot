e7a13434592a7975c9ad8df02183e777c5d72916
shingaryu <shingaryu@gmail.com>
2020-02-09 23:32:31 +0100

    Average diagonal elements and write results to csv

--- strength-table.js
+++ strength-table.js
@@ -17,6 +17,7 @@ const minimaxbot = require("./bots/minimaxbot");
 const cloneBattle = require('./util').cloneBattle;
 const importTeam = require('./util').importTeam;
 const initLog4js = require('./initLog4js');
+const moment = require('moment');
 
 // Setup Logging
 initLog4js(global.program.nolog, global.program.onlyinfo);
@@ -47,55 +48,63 @@ function makeStrengthTable() {
 
   logger.info(targetPokemons.length + ' target pokemons are loaded.');
 
-  const myTeam = [...targetPokemons];
-  const oppTeam = [...targetPokemons];
+  const targetsVertical = [...targetPokemons];
+  const targetsHorizontal = [...targetPokemons];
 	const customGameFormat = Dex.getFormat(`gen8customgame`, true);
 	customGameFormat.ruleset = customGameFormat.ruleset.filter(rule => rule !== 'Team Preview');
 
 	const oneOnOneRepetition = 1;
 	logger.info("start evaluating One-On-One strength...")
-	const evalValueTable = [];
-	myTeam.forEach(myPoke => {
-			const evalRecord = [];
-			oppTeam.forEach(oppPoke => {
-					logger.info(`evaluate about ${myPoke.name} vs ${oppPoke.name}`);
-					const repeatedOneOnOneValues = []; 
-					for (let i = 0; i < oneOnOneRepetition; i++) {
-						const p1 = { name: 'botPlayer', avatar: 1, team: [myPoke] };
-						const p2 = { name: 'humanPlayer', avatar: 1, team: [oppPoke] };								
-						const battleOptions = { format: customGameFormat, rated: false, send: null, p1, p2 };
-						const battle = new PcmBattle(battleOptions);
-						battle.start();              
-						battle.makeRequest();                   
-						const decision = BattleRoom.parseRequest(battle.p1.request);
-						const evalValue = minimaxbot.decide(cloneBattle(battle), decision.choices, false, 1, 1).tree.value;
-						repeatedOneOnOneValues.push(evalValue);
-					}
+  const evalValueTable = [];
+  for (let i = 0; i < targetsVertical.length; i++) {
+    const myPoke = targetsVertical[i];  
+    const evalRecord = [];
+    for (let j = 0; j < targetsHorizontal.length; j++) {
+      const oppPoke = targetsHorizontal[j];
+      logger.info(`evaluate about ${myPoke.name} vs ${oppPoke.name}`);
+      const repeatedOneOnOneValues = []; 
+      for (let i = 0; i < oneOnOneRepetition; i++) {
+        const p1 = { name: 'botPlayer', avatar: 1, team: [myPoke] };
+        const p2 = { name: 'humanPlayer', avatar: 1, team: [oppPoke] };								
+        const battleOptions = { format: customGameFormat, rated: false, send: null, p1, p2 };
+        const battle = new PcmBattle(battleOptions);
+        battle.start();              
+        battle.makeRequest();                   
+        const decision = BattleRoom.parseRequest(battle.p1.request);
+        const evalValue = minimaxbot.decide(cloneBattle(battle), decision.choices, false, 1, 1).tree.value;
+        repeatedOneOnOneValues.push(evalValue);
+      }
 
-					const ave = average(repeatedOneOnOneValues);
-					const stdD = stdDev(repeatedOneOnOneValues);
-					const cv = stdD / Math.abs(ave);
+      const ave = average(repeatedOneOnOneValues);
+      const stdD = stdDev(repeatedOneOnOneValues);
+      const cv = stdD / Math.abs(ave);
 
-					logger.info(`One-on-one strength: ${ave} (stddev: ${stdD}, C.V.: ${cv})`);
-					evalRecord.push(ave);
-					});
-			evalValueTable.push(evalRecord);
-	});
+      logger.info(`One-on-one strength: ${ave} (stddev: ${stdD}, C.V.: ${cv})`);
+      evalRecord.push(ave);
+    };
+
+    evalValueTable.push(evalRecord);
+  }
+
+  averageDiagonalElements(evalValueTable);
 
 	logger.debug("evaluation value table is below: ");
 	let tableHeader = '        ,';
-	oppTeam.forEach(oppPoke => {
+	targetsHorizontal.forEach(oppPoke => {
 			tableHeader += oppPoke.name + ',';
 	});
 	console.log(tableHeader);
-	for (let i = 0; i < myTeam.length; i++) {
+	for (let i = 0; i < targetsVertical.length; i++) {
 			let tableRecord = '';
-			tableRecord += myTeam[i].name + ',';
+			tableRecord += targetsVertical[i].name + ',';
 			evalValueTable[i].forEach(evalValue => {
 					tableRecord += evalValue.toFixed() + ',';
 			});
 			console.log(tableRecord);
-	}
+  }
+  
+  writeEvalTable(evalValueTable, targetsVertical.map(x => x.name), targetsHorizontal.map(x => x.name),
+    `str_table_${moment().format('YYYYMMDDHHmmss')}.csv`);
 }
 
 function stdDev(values) {
@@ -115,4 +124,44 @@ function variance(values, average) {
 	let sum = 0;
 	values.forEach(value => sum += Math.pow(value - average, 2));
 	return sum / values.length;
+}
+
+function averageDiagonalElements(evalValueTable) {
+  if (evalValueTable.some(row => row.length !== evalValueTable.length)) {
+    throw new Error('Table needs to be square');
+  }
+
+  for (let i = 0; i < evalValueTable.length; i++) {
+    for (let j = i; j < evalValueTable[i].length; j++) {
+      if (i === j) {
+        evalValueTable[i][j] = 0;
+      } else {
+        const valueIJ = evalValueTable[i][j];
+        const valueJI = evalValueTable[j][i];
+        evalValueTable[i][j] = (valueIJ - valueJI) / 2;
+        evalValueTable[j][i] = (valueJI - valueIJ) / 2;
+      }
+    }
+  }
+}
+
+function writeEvalTable(evalValueTable, rowHeader, columnHeader, filename) {
+  let csvText = '';
+  columnHeader.forEach(columnName => csvText += ','+ columnName);
+  csvText += '\n';
+
+  for (let i = 0; i < evalValueTable.length; i++) {
+    const row = evalValueTable[i];
+    for (let j = 0; j < row.length; j++) {
+      if (j === 0) {
+        csvText += rowHeader[i];
+      } 
+      
+      csvText += ',' + row[j].toFixed();
+    }
+
+    csvText += '\n';
+  }
+
+  fs.writeFileSync(filename, csvText);
 }
