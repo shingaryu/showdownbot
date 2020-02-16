3ddaf0b3f10631a092b4890620a2721202d6d262
shingaryu <shingaryu@gmail.com>
2020-02-16 11:55:54 +0100

    Validate (and also set the level to 50) imported pokemon sets

--- strength-table.js
+++ strength-table.js
@@ -11,6 +11,7 @@ global.program
 
 const fs = require('fs');
 const Dex = require('./showdown-sources/.sim-dist/dex').Dex;
+const TeamValidator = require('./showdown-sources/.sim-dist/team-validator').TeamValidator;
 const PcmBattle = require('./percymon-battle-engine').PcmBattle;
 const BattleRoom = require("./battleroom");
 const Minimax = require("./bots/minimaxbot").Minimax;
@@ -28,35 +29,51 @@ const weights = {
   "p2_hp": -1024,
 }
 
-makeStrengthTable(weights, 10, 1, 1);
+makeStrengthTable(weights, 1, 1, 1);
+// makeStrengthTable(weights, 100, 3, 1);
 
 function makeStrengthTable(weights, oneOnOneRepetition, minimaxDepth, minimaxRepetiton = 1) {
   const dirName = 'strength-table';
   const filenames = fs.readdirSync('./' + dirName);
 
+  const customGameFormat = Dex.getFormat(`gen8customgame`, true);
+  customGameFormat.ruleset = customGameFormat.ruleset.filter(rule => rule !== 'Team Preview');
+  customGameFormat.forcedLevel = 50;
+
+  const teamValidator = new TeamValidator(customGameFormat);
   const targetPokemons = [];
+
+  // Read target pokemon sets from team text. If an error occurs, just skip the file and continue.
   filenames.forEach(filename => {
     try {
       const rawText = fs.readFileSync(`./${dirName}/${filename}`, "utf8");
-      const team = importTeam(rawText); 
-      const pokemonSets = Dex.fastUnpackTeam(team);
+      const pokemonSets = importTeam(rawText); 
       if (!pokemonSets) {
         logger.warn(`'${filename}' doesn't contain a valid pokemon expression. We will just ignore this file.`);
       } else if (pokemonSets.length > 1) {
         logger.warn(`'${filename}' seems to have more than one pokemon expression. Subsequent ones are ignored.`);
-      } 
+      }
       targetPokemons.push(pokemonSets[0]);
     } catch (error) {
       logger.warn(`Failed to import '${filename}'. Is this a text of a target pokemon?`);
     }
   });
 
-  logger.info(targetPokemons.length + ' target pokemons are loaded.');
+  // Validate pokemon sets. If the validation failed about one of target pokemons, throw an exception.
+  targetPokemons.forEach(targetPokemon => {
+    const setValidationProblems = teamValidator.validateSet(targetPokemon);
+    if (setValidationProblems) {
+      logger.error(`${setValidationProblems.length} problem(s) is found about ${targetPokemon.name} during the validation.`);
+      setValidationProblems.forEach(problem => {
+        logger.error(problem);
+      })
+      throw new Error('Pokemon Set Validation Error');
+    }  
+  })
 
+  logger.info(targetPokemons.length + ' target pokemons are loaded.');
   const targetsVertical = [...targetPokemons];
   const targetsHorizontal = [...targetPokemons];
-	const customGameFormat = Dex.getFormat(`gen8customgame`, true);
-	customGameFormat.ruleset = customGameFormat.ruleset.filter(rule => rule !== 'Team Preview');
 
   logger.info("start evaluating One-On-One strength...")
   const minimax = new Minimax(false, minimaxRepetiton, false, weights);
