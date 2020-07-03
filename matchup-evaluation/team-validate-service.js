a938643ec726d79392680124e39831a72b439dfb
shingaryu <shingaryu@gmail.com>
2020-07-03 22:23:45 +0900

    Fixed logger error

--- matchup-evaluation/team-validate-service.js
+++ matchup-evaluation/team-validate-service.js
@@ -1,4 +1,5 @@
 const TeamValidator = require('../showdown-sources/.sim-dist/team-validator').TeamValidator;
+const logger = require('log4js').getLogger("bot");
 
 // Validate pokemon sets. If the validation failed about one of target pokemons, throw an exception.
 module.exports.validatePokemonSets = (gameFormat, pokemonSets) => {
