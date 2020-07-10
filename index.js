53e4aef491ba7eb4ffb52521089d8bdff9bc4cc0
shingaryu <shingaryu@gmail.com>
2020-07-11 00:18:39 +0900

    Export minimax core functions selectively as a npm package

---
+++ index.js
@@ -0,0 +1,8 @@
+// exports for external packages in which percymon is installed as a library
+module.exports.Dex = require('./showdown-sources/.sim-dist/dex').Dex;
+module.exports.TeamValidator = require('./showdown-sources/.sim-dist/team-validator').TeamValidator;
+module.exports.PcmBattle = require('./percymon-battle-engine').PcmBattle;
+module.exports.BattleRoom = require('./battleroom');
+module.exports.Minimax = require('./bots/minimaxbot').Minimax;
+module.exports.Util = require('./util');
+module.exports.initLog4js = require('./initLog4js');
