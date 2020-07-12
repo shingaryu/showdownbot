e6fb5ab757cbc154627a78217043509207258fc7
shingaryu <shingaryu@gmail.com>
2020-07-12 23:52:16 +0900

    Update library exports

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ module.exports.BattleRoom = require('./battleroom');
 module.exports.Minimax = require('./bots/minimaxbot').Minimax;
 module.exports.Util = require('./util');
 module.exports.initLog4js = require('./initLog4js');
+module.exports.TeamImporter = require('./team-importer').TeamImporter;
