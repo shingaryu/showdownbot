5907815c1f82d8702a33c595e8aa23ba78ad819f
shingaryu <shingaryu@gmail.com>
2020-11-26 21:39:07 +0100

    revert the implementation in index.js for backward compatibility

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ module.exports.TeamValidator = require('./showdown-sources/.sim-dist/team-valida
 module.exports.PcmBattle = require('./percymon-battle-engine').PcmBattle;
 module.exports.Minimax = require('./bots/minimaxbot').Minimax;
 module.exports.Util = require('./util');
-module.exports.configureLogger = require('./configure-logger');
+// for keeping backward compatibility in v3.X.X
+// module.exports.configureLogger = require('./configure-logger');
+module.exports.initLog4js = (nolog, onlyinfo) => require('./configure-logger')(onlyinfo ? 'info': 'all', !nolog);
 module.exports.TeamImporter = require('./team-importer').TeamImporter;
